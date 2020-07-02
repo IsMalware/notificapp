2a4b992c623916e92327ab64fc8ac206ad0e8538
IsMalware <Ismaellaquino@hotmail.com>
2020-07-02 16:44:31 -0300

    Alterando arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -20,57 +20,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // Inicializa o Dio para ser usado no metodo POST
-  final Dio dio = Dio();
-  // Define a url da api google para FCM
-  var url = 'https://fcm.googleapis.com/fcm/send';
-  // declara e inicia a variável que exibira o token
-  var _homeScreenText = "Esperando o Token...";
-  // cria uma instancia do FCM
+  final controllerTitle = TextEditingController();
+  final controllerBody = TextEditingController();
+  final controllerUsers = TextEditingController();
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  // Declara os controllers para serem usados nas mensagens
-  final _controllerTitle = TextEditingController();
-  final _controllerBody = TextEditingController();
-  final _controllerUsers = TextEditingController();
 
-// Define uma lista de usuários estática.
-  var users = <String, String>{
-    'ismael':
-        'eG1PIKS2QhSs3RVJqW1zBK:APA91bEpkK35uhEiEjtc2D5VnBCm48oXbcqedscpgz_Ho2DjWuOpfpukJr7TCPrxG1HZ1f1TKlrF0J-QPVnC7cBu2rLeegN78PbTeJwk_X9TTe5OFcDjPerbJGWOFfxCjASIYBhk-cuU',
-  };
+  Response response = Response();
+  Stopwatch stopwatch = Stopwatch();
 
-  // Desnecessario, mas serve para pegar o token do usuário
-  Future<String> get userToken => _firebaseMessaging.getToken();
+  var time = 0;
+  var totalNotifica = 0;
+  var _token;
+  var homeScreenText;
 
   @override
   void initState() {
+    _firebaseMessaging.getToken().then((token) {
+      setState(() {
+        _token = token;
+        assert(_token != null);
+        homeScreenText = 'Seu Token é: $token';
+        print(homeScreenText);
+      });
+    });
     super.initState();
-    // Requisita as permissões de notificação para o IOS, desnecessaria, nesse caso,
-    // pois é somente para android.
     _firebaseMessaging.requestNotificationPermissions(
         const IosNotificationSettings(sound: true, alert: true, badge: true));
     _firebaseMessaging.onIosSettingsRegistered
         .listen((IosNotificationSettings settings) {
       print('Settings registred: $settings');
     });
-    // Exibe o token do usuario
-    _firebaseMessaging.getToken().then((String token) {
-      assert(token != null);
-      setState(() {
-        _homeScreenText = 'Seu Token é: $token';
-      });
-      print(_homeScreenText);
-      // aqui se configura o que acontece quando uma mensage for recebida, lançada ou clicada
-      _firebaseMessaging.configure(
-        onMessage: (Map<String, dynamic> message) async {
-          print('onMessage: $message');
-          setState(() {
-            // muda o estado do homeScreenText para exibir a mensagem.
-            _homeScreenText =
-                'Titulo da mensagem: ${message['notification']['title']}\nA mensagem enviada é: ${message['notification']['body']}';
-          });
-        },
-      );
+    // aqui se configura o que acontece quando uma mensage for recebida, lançada ou clicada
+    setState(() {
+      configureFireMsg(_firebaseMessaging, homeScreenText);
     });
   }
 
@@ -78,81 +60,114 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Teste'),
-          centerTitle: true,
-        ),
-        body: Material(
-          child: Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        appBar: _appBar('Testando'),
+        body: _body(),
+      ),
+    );
+  }
+
+  Map<String, dynamic> configureFireMsg(
+      FirebaseMessaging _firebaseMessaging, String homeScreenText) {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        return homeScreenText =
+            'Titulo da mensagem: ${message['notification']['title']}\nA mensagem enviada é: ${message['notification']['body']}';
+      },
+    );
+    return null;
+  }
+
+  Widget _appBar(String title) {
+    return AppBar(
+      title: Text('$title'),
+      centerTitle: true,
+    );
+  }
+
+  Widget _form(String labelText, TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: '$labelText',
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(15),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _form('Para:', controllerUsers),
+              _form('Titulo: ', controllerTitle),
+              _form('Mensagem', controllerBody),
+              RaisedButton.icon(
+                onPressed: () {
+                  stopwatch.start();
+                  notifica();
+                  setState(() {
+                    time = stopwatch.elapsed.inMilliseconds;
+                  });
+                  stopwatch.stop();
+                },
+                icon: Icon(Icons.email),
+                label: Text(
+                  'Enviar notificação!',
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  TextFormField(
-                    // utiliza-se o controller anteriormente criado para capturar
-                    // o texto escrito pelo usuário.
-                    controller: _controllerUsers,
-                    autocorrect: true,
-                    decoration: InputDecoration(
-                      labelText: 'To',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _controllerTitle,
-                    decoration: InputDecoration(
-                      labelText: 'Titulo',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _controllerBody,
-                    decoration: InputDecoration(
-                      labelText: 'Mensagem',
-                    ),
-                  ),
-                  RaisedButton.icon(
-                    onPressed: () async {
-                      // incrementamos a mensagem que sera enviada com os dados
-                      // fornecidos pelo usuário, no TextFormField.
-                      dynamic menssagem = <String, dynamic>{
-                        "to": "${users['${_controllerUsers.text}']}",
-                        "notification": {
-                          "title": "${_controllerTitle.text}",
-                          "body": "${_controllerBody.text}",
-                        },
-                      };
-                      // definimos o header com a key do servidor do FCM
-                      dynamic headers = <String, dynamic>{
-                        "Authorization": "key=SuaKey",
-                      };
-                      // Cria a requisição HTTP de metodo POST para o server GoogleAPI
-                      Response response = await dio.post(
-                        url,
-                        data: menssagem,
-                        options: Options(
-                          headers: headers,
-                          contentType: "application/json",
-                        ),
-                      );
-                      // Desnecessario, mas serve para verificar se o _controllerUser
-                      // está retornando o valor correto
-                      print(
-                          '$response |||||||| ${users['${_controllerUsers.text}']}');
-                    },
-                    icon: Icon(Icons.email),
-                    label: Text(
-                      'Enviar menssagem!',
-                    ),
-                  ),
-                  Text(
-                    '$_homeScreenText',
-                    textAlign: TextAlign.center,
-                  ),
+                  Text('$totalNotifica Notificações'),
+                  Text(time.toString() + 'ms'),
                 ],
               ),
-            ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void notifica() async {
+    final Dio dio = Dio();
+    var url = 'https://fcm.googleapis.com/fcm/send';
+
+    var users = <String, String>{
+      'ismael':
+          'G1PIKS2QhSs3RVJqW1zBK:APA91bEpkK35uhEiEjtc2D5VnBCm48oXbcqedscpgz_Ho2DjWuOpfpukJr7TCPrxG1HZ1f1TKlrF0J-QPVnC7cBu2rLeegN78PbTeJwk_X9TTe5OFcDjPerbJGWOFfxCjASIYBhk-cuU',
+      'joão': 'awdomjnahsdnjwmkladl',
+    };
+
+    dynamic menssagem = <String, dynamic>{
+      'to': '${users['${controllerUsers.text}']}',
+      'notification': {
+        'title': '${controllerTitle.text}',
+        'body': '${controllerBody.text}',
+      },
+    };
+    dynamic headers = <String, dynamic>{
+      "Authorization":
+          "key=AAAAeEBcmts:APA91bF_XffpBkECEOMPYuddDp39w27Q0jBat3fUskE83h8SODwqRQHxkzRftP0r0BLiAFiu-s-fI3_0Ahy5ktNs2iAKVlimDcKAsisCF5gZxWGKnhShCuX5vnDp8eK1s-orqQfSQN6Z",
+    };
+    response = await dio.post(
+      url,
+      data: menssagem,
+      options: Options(
+        headers: headers,
+        contentType: "application/json",
+      ),
+    );
+    if (response.statusCode != 200) {
+      return null;
+    } else {
+      setState(() {
+        totalNotifica++;
+      });
+    }
   }
 }
